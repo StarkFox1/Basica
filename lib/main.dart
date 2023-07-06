f54b4298df47eb7545d117aa431c85783eefb77e
Jacob Eldhose <u2008035@rajagiri.edu.in>
2023-07-06 20:24:01 +0530

    basica has been created

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:basica/screens/splash.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+const Save_key = 'UserLoggedIn';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Login Page',
+        theme: ThemeData(primarySwatch: Colors.green),
+        home: screenSplash());
+  }
+}
